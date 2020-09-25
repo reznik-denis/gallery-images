8962dbe5c932cb8ecf1dc73ca0861d52dc89c86a
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-09-25 21:50:41 +0300

    изменения

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,4 @@
 import gallerysItems from './gallery-items.js'
-{/* <li class="gallery__item">
-    <a
-        class="gallery__link"
-        href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-    >
-        <img
-            class="gallery__image"
-            src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-            alt="Tulips"
-        />
-    </a>
-</li> */}
 
 const galerysEl = ({ preview, original, description }) => {
     return `<li class="gallery__item">
